91c7cb3f1a08c93c922931f08a02416d6f02114e
326623 <yangqp5@mail2.sysu.edu.cn>
2018-02-11 21:04:13 +0800

    adding frontend

--- Vagrantfile
+++ Vagrantfile
@@ -15,6 +15,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network "forwarded_port", guest: 5000, host: 5000
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 # notive that it will prompt for password
-apt install -y python3 python3-pip language-pack-zh-hans mysql-server
+apt install -y python3 python3-pip language-pack-zh-hans mysql-server npm
 
 mysql < $DB/crew-user.sql
 # preparing flask restplus
@@ -24,11 +24,16 @@ pip3 install -e $PIP_SETUP
 # setting envvar
 echo 'export FLASK_APP=crew_api' >> /home/vagrant/.bashrc
 echo 'export FLASK_DEBUG=1' >> /home/vagrant/.bashrc
+echo 'export LC_ALL=zh_CN.UTF-8' >> /home/vagrant/.bashrc
 
 # moving mysql test database for testing acceleration
 service mysql stop
 ln -s /run/test /var/lib/mysql/test/
 service mysql start
+
+npm install -g n
+n stable
+npm install -g vue-cli
 
 # if ! [ -L /var/www ]; then
 #     rm -rf /var/www
